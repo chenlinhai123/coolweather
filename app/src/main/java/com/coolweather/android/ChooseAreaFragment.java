fd08c0820241881f21c6443187d0157ec76f4bc0
chenlinhai <chenlinhai@sagereal.com>
2019-09-07 10:14:55 +0800

    完成遍历省市县三级列表的功能。
    Change-Id: Ic5183d609cb080763a6b9c480bf42705e3e150e8

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -193,9 +193,9 @@ public class ChooseAreaFragment extends Fragment {
      * 根据传入的地址和类型从服务器上查询省市县数据
      * */
 
-    private void queryFromServer(String addrss,final String type){
+    private void queryFromServer(String address,final String type){
         showProgressDialog();
-        HttpUtil.sendOkHttpReQuest(addrss, new Callback() {
+        HttpUtil.sendOkHttpReQuest(address, new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
                 //通过runOnUiThread()方法回到主线程处理逻辑
