2eb9340357c0ca5e55fe393bfcb621c7b0205d13
chenlinhai <chenlinhai@sagereal.com>
2019-09-07 15:56:17 +0800

    待续。
    Change-Id: I04b8e16b1ca7007fa0855c404005e92e3ba93d24

---
+++ app/src/main/java/com/coolweather/android/gson/Basic.java
@@ -0,0 +1,20 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Basic {
+
+    @SerializedName("city")
+    public String cityName;
+
+    @SerializedName("id")
+    public String weatherId;
+
+    public Update upddate;
+
+    public class Update{
+
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
